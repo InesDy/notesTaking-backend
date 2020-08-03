9b887c501092359432a196601e417614fc8b057a
Ines Dyll <inesdyll1@gmail.com>
2020-08-03 16:25:12 +0200

    initial commit

---
+++ notes-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '43ebb823c757bf10807ddaf2201be356'),
+    },
+  },
+});
---
+++ notes-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '051ff4f6-854b-4fe5-b69b-1330bcb9e39d'
+};
