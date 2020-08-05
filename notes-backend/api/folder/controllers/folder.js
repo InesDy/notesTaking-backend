ca9b37a034b4624fc217e58e7294bd12bb99e589
Ines Dyll <inesdyll1@gmail.com>
2020-08-05 15:14:18 +0200

    queries added

--- notes-backend/api/folder/controllers/folder.js
+++ notes-backend/api/folder/controllers/folder.js
@@ -1,8 +1,100 @@
-'use strict';
-
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    //FIND ALL
+
+    ctx.query.user = ctx.state.user.id;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.folder.search(ctx.query);
+    } else {
+      entities = await strapi.services.folder.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      const folder = sanitizeEntity(entity, {
+        model: strapi.models.folder,
+      });
+
+      return folder;
+    });
+  },
+
+  // FIND ONE
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.folder.findOne({
+      id,
+      "user.id": ctx.state.user.id,
+    });
+    const folder = sanitizeEntity(entity, { model: strapi.models.folder });
+
+    return folder;
+  },
+
+  // CREATE
+  //ctx has query and state (info about user)
+  // we pass the  user id to  the database query
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.folder.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.folder.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.folder });
+  },
+
+  //UPDATE
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [folder] = await strapi.services.folder.find({
+      id,
+      "user.id": ctx.state.user.id,
+    });
+
+    if (!folder) {
+      return ctx.unuserized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.folder.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.folder.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.note });
+  },
+
+  // DELETE
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.folder.delete({
+      id,
+      "user.id": ctx.state.user.id,
+    });
+    return sanitizeEntity(entity, { model: strapi.models.note });
+  },
+};
--- notes-backend/api/note/controllers/note.js
+++ notes-backend/api/note/controllers/note.js
@@ -1,8 +1,100 @@
-'use strict';
-
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    //FIND ALL
+
+    ctx.query.user = ctx.state.user.id;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.note.search(ctx.query);
+    } else {
+      entities = await strapi.services.note.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      const note = sanitizeEntity(entity, {
+        model: strapi.models.note,
+      });
+
+      return note;
+    });
+  },
+
+  // FIND ONE
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.note.findOne({
+      id,
+      "user.id": ctx.state.user.id,
+    });
+    const note = sanitizeEntity(entity, { model: strapi.models.note });
+
+    return note;
+  },
+
+  // CREATE
+  //ctx has query and state (info about user)
+  // 36 we pass the  user id to  the database query
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.note.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.note.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.note });
+  },
+
+  //UPDATE
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [note] = await strapi.services.note.find({
+      id,
+      "user.id": ctx.state.user.id,
+    });
+
+    if (!note) {
+      return ctx.unuserized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.note.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.note.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.note });
+  },
+
+  // DELETE
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.note.delete({
+      id,
+      "user.id": ctx.state.user.id,
+    });
+    return sanitizeEntity(entity, { model: strapi.models.note });
+  },
+};
